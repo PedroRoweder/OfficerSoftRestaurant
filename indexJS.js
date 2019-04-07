b72d172c1e35f5ae85dff36287f3aa051c5fdeb1
Pedro Roweder <42496316+PedroRoweder@users.noreply.github.com>
2019-04-07 20:34:11 -0300

    Add files via upload

--- indexJS.js
+++ indexJS.js
@@ -1,1 +1,19 @@
 
+$(document).ready(function() {
+    $('#checkin').on('click', function adicionaMesa(){
+        let numeroDaMesa = document.getElementById("mesa").value;
+        let numeroDePessoas = document.getElementById("clientes").value;
+
+        $('#mesa' + numeroDaMesa).css("background-color", "#db5243");
+        $('#mesa' + numeroDaMesa).css("border-color", "#962a1e")
+        $('#mesa' + numeroDaMesa).hover(function(){
+            $('#mesa' + numeroDaMesa).css("background-color", "#e47366");
+        },function(){
+            $('#mesa' + numeroDaMesa).css("background-color", "#db5243");
+        });
+
+        $('#pessoas' + numeroDaMesa).html(numeroDePessoas);
+    });
+
+    
+});
