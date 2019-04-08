5473710fe6784f914cdc753e5de9ca724c4f86b2
Pedro Roweder <42496316+PedroRoweder@users.noreply.github.com>
2019-04-07 21:11:12 -0300

    Add files via upload

--- indexJS.js
+++ indexJS.js
@@ -1,8 +1,22 @@
 
 $(document).ready(function() {
+
+    let pessasNoRestaurante = 0;
+    let mesasLivres = 10;
+    let mesasOcupadas = 0;
+    let mesas = [0,0,0,0,0,0,0,0,0,0];
+    console.log(mesasLivres);
+
     $('#checkin').on('click', function adicionaMesa(){
-        let numeroDaMesa = document.getElementById("mesa").value;
+        let numeroDaMesa = document.getElementById("mesaCheckin").value;
         let numeroDePessoas = document.getElementById("clientes").value;
+
+        pessasNoRestaurante = pessasNoRestaurante + parseInt(numeroDePessoas);
+        mesasOcupadas++;
+        mesasLivres--;
+        $('#mesasLivres').html(mesasLivres);
+        $('#mesasOcupadas').html(mesasOcupadas);
+        $('#pessoasNoRestaurante').html(pessasNoRestaurante);
 
         $('#mesa' + numeroDaMesa).css("background-color", "#db5243");
         $('#mesa' + numeroDaMesa).css("border-color", "#962a1e")
@@ -15,5 +29,13 @@ $(document).ready(function() {
         $('#pessoas' + numeroDaMesa).html(numeroDePessoas);
     });
 
-    
+    $('#submitPedidos').on('click', function adicionar(){
+        let numeroDaMesa = parseInt(document.getElementById("mesaPedidos").value);
+        let valorDoPedido = parseInt(document.getElementById("listaPedidos").value);
+
+        mesas[numeroDaMesa - 1] += valorDoPedido;
+
+        $('#valor' + numeroDaMesa).html(mesas[numeroDaMesa - 1]);
+
+    });
 });
