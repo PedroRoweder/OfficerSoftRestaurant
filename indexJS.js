37995d2b7a66ed532896025b26aa5a8be557c282
Pedro Roweder <42496316+PedroRoweder@users.noreply.github.com>
2019-04-07 21:41:21 -0300

    Add files via upload

--- indexJS.js
+++ indexJS.js
@@ -6,37 +6,43 @@ $(document).ready(function() {
     let mesasOcupadas = 0;
     let dinheiroReceber = 0;
     let mesas = [0,0,0,0,0,0,0,0,0,0];
+    let ocupacao = [0,0,0,0,0,0,0,0,0,0];
 
     $('#checkin').on('click', function adicionaMesa(){
         let numeroDaMesa = document.getElementById("mesaCheckin").value;
         let numeroDePessoas = document.getElementById("clientes").value;
 
-        pessasNoRestaurante = pessasNoRestaurante + parseInt(numeroDePessoas);
-        mesasOcupadas++;
-        mesasLivres--;
-        $('#mesasLivres').html(mesasLivres);
-        $('#mesasOcupadas').html(mesasOcupadas);
-        $('#pessoasNoRestaurante').html(pessasNoRestaurante);
-
-        $('#mesa' + numeroDaMesa).css("background-color", "#db5243");
-        $('#mesa' + numeroDaMesa).css("border-color", "#962a1e");
-        $('#mesa' + numeroDaMesa).hover(function(){
-            $('#mesa' + numeroDaMesa).css("background-color", "#e47366");
-        },function(){
+        if(!ocupacao[numeroDaMesa - 1]) {
+            ocupacao[numeroDaMesa - 1] = 1;
+            pessasNoRestaurante = pessasNoRestaurante + parseInt(numeroDePessoas);
+            mesasOcupadas++;
+            mesasLivres--;
+            $('#mesasLivres').html(mesasLivres);
+            $('#mesasOcupadas').html(mesasOcupadas);
+            $('#pessoasNoRestaurante').html(pessasNoRestaurante);
             $('#mesa' + numeroDaMesa).css("background-color", "#db5243");
-        });
+            $('#mesa' + numeroDaMesa).css("border-color", "#962a1e");
+            $('#mesa' + numeroDaMesa).hover(function(){
+                $('#mesa' + numeroDaMesa).css("background-color", "#e47366");
+            },function(){
+                $('#mesa' + numeroDaMesa).css("background-color", "#db5243");
+            });
 
-        $('#pessoas' + numeroDaMesa).html(numeroDePessoas);
+            $('#pessoas' + numeroDaMesa).html(numeroDePessoas);
+
+    }else { alert("Esta mesa já está ocupada!"); }
     });
 
     $('#submitPedidos').on('click', function adicionar(){
         let numeroDaMesa = parseInt(document.getElementById("mesaPedidos").value);
         let valorDoPedido = parseInt(document.getElementById("listaPedidos").value);
 
+        if(ocupacao[numeroDaMesa -1]) {
         dinheiroReceber += valorDoPedido;
         mesas[numeroDaMesa - 1] += valorDoPedido;
 
         $('#valor' + numeroDaMesa).html(mesas[numeroDaMesa - 1]);
         $('#dinheiroReceber').html(dinheiroReceber);
+        }else { alert("Não é possivel adicionar um pedido a uma mesa vazia!"); }
     });
 });
